041a6c2352815af0f412d9f4b09917db78f2aaf8
Guillaume <gubersac@gmail.com>
2021-08-30 15:28:12 +0200

    optimization to make the function work with 25 lines

--- src/main/scala/app/MinTrianglePath.scala
+++ src/main/scala/app/MinTrianglePath.scala
@@ -1,47 +1,55 @@
 package app
 
 import scala.io.StdIn.readLine
-
+import scala.collection.mutable.{Map => MMap}
 sealed trait Triangle
-final case class TriangleNode(value: Int, left: Triangle, right: Triangle) extends Triangle
+final case class TriangleNode(value: Long, left: Triangle, right: Triangle) extends Triangle
 final object TriangleEnd extends Triangle
 
 object Triangle {
-  /**
-    * TODO optimization: each node is instantiated twice (once by parent left and once by parent right)
-    * @return the head of the corresponding triangle
-    */
-  def apply(input: Seq[Seq[Int]]): Triangle = {
-    def loop(lin: Int, col: Int): Triangle =
-      if (lin < input.length && col < input(lin).length)
-        TriangleNode(input(lin)(col),
-          loop(lin + 1, col), 
-          loop(lin + 1, col + 1)
-        )
+
+  def apply(input: Seq[Seq[Long]]): Triangle = {
+    def loop(lin: Int, col: Int, leftValue: Option[Triangle]): Triangle =
+      if (lin < input.length && col < input(lin).length) {
+        val left = leftValue.getOrElse(loop(lin + 1, col, None))
+        val right = left match {
+          case TriangleNode(_, _, rightValue) => 
+            loop(lin + 1, col + 1, Some(rightValue))
+          case TriangleEnd => loop(lin + 1, col + 1, None)
+        }
+        TriangleNode(input(lin)(col), left, right)
+      }
       else TriangleEnd
 
-    loop(0, 0)
+    loop(0, 0, None)
   }
 
-  def findMinPath(triangle: TriangleNode): List[Int] = 
-    (triangle.left, triangle.right) match {
-      case (left @ TriangleNode(_, _, _), right @ TriangleNode(_, _, _)) =>
-        val l, lp = findMinPath(left)
-        val r, rp = findMinPath(right)
-        if (lp.sum < rp.sum) triangle.value :: lp
-        else triangle.value :: rp
-      case _ =>
-        List(triangle.value)
-    }
+  def findMinPath(triangle: TriangleNode): List[Long] = {
+    // that mutable map does not break functionnal programming principle because the function is still pure
+    val computed: MMap[TriangleNode, List[Long]] = MMap.empty
+    def loop(triangle: TriangleNode): List[Long] =
+      if (computed.contains(triangle)) computed(triangle)
+      else (triangle.left, triangle.right) match {
+        case (left @ TriangleNode(_, _, _), right @ TriangleNode(_, _, _)) =>
+          val l, lp = loop(left)
+          val r, rp = loop(right)
+          val res = if (lp.sum < rp.sum) triangle.value :: lp else triangle.value :: rp
+          computed.addOne((triangle, res))
+          res
+        case _ =>
+          List(triangle.value)
+      }
+    loop(triangle)
+  }
 
 }
 
 object MinTrianglePath extends App {
 
   // 1- read from stdin - I made the assumption that the input is always correct
-  val input: Seq[Seq[Int]] = Iterator.continually(readLine)
+  val input: Seq[Seq[Long]] = Iterator.continually(readLine)
     .takeWhile(_ != null)
-    .map(s => s.split(" ").map(_.toInt).toList)
+    .map(s => s.split(" ").map(_.toLong).toList)
     .toList
 
   // 2- parse as a triangle
--- src/test/scala/app/MinTrianglePathSpec.scala
+++ src/test/scala/app/MinTrianglePathSpec.scala
@@ -1,11 +1,12 @@
 package app
 
+import scala.io.Source
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class ParseSpec extends AnyFlatSpec with Matchers {
 
-  val exampleList = List(
+  val exampleList: List[List[Long]] = List(
     List(7),
     List(6, 3),
     List(3, 8, 5),
@@ -25,14 +26,57 @@ class ParseSpec extends AnyFlatSpec with Matchers {
     )
   }
 
-  "Triangle.findMinPath" should "parse as expected" in {
+  "Triangle.findMinPath" should "work as expected" in {
     Triangle(exampleList) match {
       case t @ TriangleNode(_, _, _) => 
         Triangle.findMinPath(t) shouldEqual List(7, 6, 3, 2)
       case _ =>
         fail("Parsing error")
+    }    
+  }
+
+  def time[R](block: => R): R = {
+    val t0 = System.nanoTime()
+    val result = block    // call-by-name
+    val t1 = System.nanoTime()
+    println("elapsed time: " + ((t1 - t0) / 1000000) + "ms")
+    result
+  }
+
+  def readFromFile(path: String) =
+    Source.fromFile(path)
+      .getLines
+      .map(s => s.split(" ").map(_.toLong).toList)
+      .toList
+
+  "Triangle.findMinPath" should "work as expected even for a 15 line input" in {
+    println("For a 15 line triangle")
+    print("For parsing ")
+    val input = time { readFromFile("testData/15.txt") }
+    Triangle(input) match {
+      case t @ TriangleNode(_, _, _) => 
+        print("For solution ")
+        val res = time { Triangle.findMinPath(t) }
+        res shouldEqual List(74, 54, 80, 32, 23, 43, 51, 18, 31, 33, 25, 12, 18, 11, 35) // = 540
+      case _ =>
+        fail("Parsing error")
     }
-    
+  }
+
+  "Triangle.findMinPath" should "work as expected even for a 25 line input" in {
+    println("For a 25 line triangle")
+    print("For parsing ")
+    val input = time { readFromFile("testData/25.txt") }
+    Triangle(input) match {
+      case t @ TriangleNode(_, _, _) => 
+        print("For solution ")
+        val res = time { Triangle.findMinPath(t) } // we are just testing if the program finish at all
+        res shouldEqual List(
+          74, 54, 80, 32, 23, 43, 51, 18, 31, 33, 25, 12, 18, 11, 35, 16, 12, 30, 14, 39, 77, 11, 79, 12, 30
+        ) // = 860
+      case _ =>
+        fail("Parsing error")
+    }
   }
 
 }
