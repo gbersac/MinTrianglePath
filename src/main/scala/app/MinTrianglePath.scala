f00008cbb41e2ae76e79b15d3cd4a9f7ac08ad52
Guillaume <gubersac@gmail.com>
2021-08-30 12:02:39 +0200

    rename tree as triangle

--- src/main/scala/app/MinTrianglePath.scala
+++ src/main/scala/app/MinTrianglePath.scala
@@ -2,41 +2,53 @@ package app
 
 import scala.io.StdIn.readLine
 
-sealed trait Tree
-final case class TreeNode(value: Int, left: Tree, right: Tree) extends Tree
-final object TreeEnd extends Tree
+sealed trait Triangle
+final case class TriangleNode(value: Int, left: Triangle, right: Triangle) extends Triangle
+final object TriangleEnd extends Triangle
 
-object Tree {
-  // I made the assumption that the input is always correct and there is always at least one line
-  // TODO optimization: each node is instantiated twice (once by parent left and once by parent right)
-  def apply(input: Seq[Seq[Int]]): Tree = {
-    def loop(lin: Int, col: Int): Tree =
+object Triangle {
+  /**
+    * TODO optimization: each node is instantiated twice (once by parent left and once by parent right)
+    * @return the head of the corresponding triangle
+    */
+  def apply(input: Seq[Seq[Int]]): Triangle = {
+    def loop(lin: Int, col: Int): Triangle =
       if (lin < input.length && col < input(lin).length)
-        TreeNode(input(lin)(col),
+        TriangleNode(input(lin)(col),
           loop(lin + 1, col), 
           loop(lin + 1, col + 1)
         )
-      else TreeEnd
+      else TriangleEnd
 
     loop(0, 0)
   }
+
+  /*
+  def findMinPath(triangle: TriangleNode): List[Int] = {
+    if (Triangle.left != TriangleEnd && Triangle.right != TriangleEnd) {
+      val l, lp = findMinPath()
+
+    }
+    else List(Triangle.value)
+  }*/
 
 }
 
 object MinTrianglePath extends App {
 
-  // 1- read from stdin
-  val input = Iterator.continually(readLine)
+  // 1- read from stdin - I made the assumption that the input is always correct
+  val input: Seq[Seq[Int]] = Iterator.continually(readLine)
     .takeWhile(_ != null)
     .mkString("\n")
     .split("\n")
-    .map(s => s.split(" ").map(_.toInt))
+    .map(s => s.split(" ").map(_.toInt).toList)
     .toList
 
-  // 2- parse as a binary tree
-  // val tree = Tree(input)
+  // 2- parse as a binary Triangle
+  val triangle = Triangle(input)
 
   // 3- find the minimal path
+
   // 4- print the minimal path to stdout
 
 }
--- src/test/scala/app/MinTrianglePathSpec.scala
+++ src/test/scala/app/MinTrianglePathSpec.scala
@@ -4,21 +4,21 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class ParseSpec extends AnyFlatSpec with Matchers {
-  "The tree parser" should "parse as expected" in {
+  "The triangle parser" should "parse as expected" in {
     val l = List(
       List(7),
       List(6, 3),
       List(3, 8, 5),
       List(11, 2, 10, 9),
     )
-    Tree(l) shouldEqual TreeNode(7,
-      TreeNode(6,
-        TreeNode(3, TreeNode(11, TreeEnd, TreeEnd), TreeNode(2, TreeEnd, TreeEnd)),
-        TreeNode(8, TreeNode(2, TreeEnd, TreeEnd), TreeNode(10, TreeEnd, TreeEnd))
+    Triangle(l) shouldEqual TriangleNode(7,
+      TriangleNode(6,
+        TriangleNode(3, TriangleNode(11, TriangleEnd, TriangleEnd), TriangleNode(2, TriangleEnd, TriangleEnd)),
+        TriangleNode(8, TriangleNode(2, TriangleEnd, TriangleEnd), TriangleNode(10, TriangleEnd, TriangleEnd))
       ),
-      TreeNode(3,
-        TreeNode(8, TreeNode(2, TreeEnd, TreeEnd), TreeNode(10, TreeEnd, TreeEnd)),
-        TreeNode(5, TreeNode(10, TreeEnd, TreeEnd), TreeNode(9, TreeEnd, TreeEnd))
+      TriangleNode(3,
+        TriangleNode(8, TriangleNode(2, TriangleEnd, TriangleEnd), TriangleNode(10, TriangleEnd, TriangleEnd)),
+        TriangleNode(5, TriangleNode(10, TriangleEnd, TriangleEnd), TriangleNode(9, TriangleEnd, TriangleEnd))
       )
     )
   }
